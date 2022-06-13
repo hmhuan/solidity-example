c9549b181d90681044dd27226bb82d6d0c309134
Minh-Huan Huynh <1612858@student.hcmus.edu.vn>
2022-06-13 16:56:02 +0900

    Error handling

---
+++ Account.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.13;
+
+contract Account {
+    uint public balance;
+    uint public constant MAX_UINT = 2 ** 256 - 1;
+
+    function deposit(uint _amount) public {
+        uint oldBalance = balance;
+        uint newBalance = balance + _amount;
+
+        require(newBalance >= oldBalance, "Overflow");
+
+        balance = newBalance;
+
+        assert(balance >= oldBalance);
+    }
+
+    function widthdraw(uint _amount) public {
+        uint oldBalance = balance;
+
+        require(balance >= _amount, "Underflow");
+
+        balance -= _amount;
+
+        assert(balance <= oldBalance);
+    }
+}
