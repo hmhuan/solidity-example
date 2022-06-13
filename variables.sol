1cc21fc50417d73d3dc0be77a8b3f9e9562e336d
Minh-Huan Huynh <1612858@student.hcmus.edu.vn>
2022-06-13 11:38:39 +0900

    constant

--- variables.sol
+++ variables.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.8.13;
 
+
+// local: 
+//   declare inside a function
+//   not stored on the blockchain
+// state:
+//   declared outside function (in contract)
+//   stored on the blockchain
+// global: provides information about the blockchain  
 contract Variables {
     // state variables are stored on the blockchain
     string public text = "Hello";
@@ -13,4 +21,9 @@ contract Variables {
         uint timestamp = block.timestamp; // current block timestamp
         address sender = msg.sender; // address of the caller
     }
+
+    // constant cannot be modified
+    // their value is hardcoded and using constant can save gas cost
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_INT = 1234;
 }
