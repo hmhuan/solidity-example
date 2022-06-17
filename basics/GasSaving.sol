1a2fd868ea245bbae50e4a6795dc99e842f2fdb0
Minh-Huan Huynh <1612858@student.hcmus.edu.vn>
2022-06-17 18:01:37 +0900

    gas saving techniques

---
+++ basics/GasSaving.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.13;
+
+/*
+some gas saving techniques:
+    replacing memory with calldata
+    loading state variable to memory
+    replacing for loop i++ with ++i
+    caching array elements
+    short circuit
+*/
+
+contract GasGolf {
+    uint public total;
+
+    function sumIfEvenAndLessThan99(uint[] calldata nums) external {
+        uint _total = total;
+        uint len = nums.lenght;
+
+        for (uint i=0; i < len; ) {
+            uint num = nums[i];
+            if (num < 99 && num % 2 == 0) {
+                _total += num;
+            }
+
+            unchecked { // checked is default - unchecked block to save gas
+                ++i;
+            }
+        }
+        total = _total;
+    }
+}
